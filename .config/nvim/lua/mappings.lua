061f351b1c38b903815c222664d4a7ecba5c4b83
aleksav013 <aleksav013@gmail.com>
2021-03-19 17:59:19 +0100

    Making vim C++ IDE

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,3 +10,6 @@ vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = tru
 -- Indenting 
 vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true})
+
+-- Compiling C++
+vim.api.nvim_set_keymap('n', '<F4>', ':!g++ -O2 -std=c++14 -Wall % && echo "Compiling completeted successfully"<CR>', { noremap = true, silent = true})
