c38a7bfd9f0aeb259a3636cce591e5cd385f8b7f
Aleksa Vučković <aleksav013@gmail.com>
2022-03-06 12:51:54 +0100

    neomutt + wpa_supplicant + pacman.conf

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-stow --no-folding --ignore="laptop|.sh" -t ~ .
+stow --no-folding --ignore="laptop|artix|.sh" -t ~ .
+doas stow --no-folding --ignore=".conf" -t "/usr/share/dbus-1/system-services/" artix
+doas stow --no-folding --ignore=".service" -t "/etc/" artix
 doas stow --no-folding --ignore=".rules" -t /etc/X11/xorg.conf.d laptop
 doas stow --no-folding --ignore=".conf" -t /etc/udev/rules.d laptop
