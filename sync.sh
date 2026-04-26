57c94ec9a39d314020dbb98b5e41e79d06589c97
Aleksa Vuckovic <aleksa@vuckovic.cc>
2026-04-26 12:24:56 +0200

    remove sbctl hook

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ rm -f ~/.config/mimeapps.list
 stow --no-folding --ignore="laptop|artix|hooks|\.sh" -t ~ .
 doas stow --no-folding --ignore="\.rules" -t /etc/X11/xorg.conf.d laptop
 doas stow --no-folding --ignore="\.conf" -t /etc/udev/rules.d laptop
-doas stow --no-folding -t /etc/pacman.d/hooks hooks
+#doas stow --no-folding -t /etc/pacman.d/hooks hooks
